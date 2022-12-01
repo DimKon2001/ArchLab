2ef588a4bd9f9fe380b67cbe1b7dfd9bc5db0bc6
kprattis <68779380+kprattis@users.noreply.github.com>
2022-12-01 11:29:32 +0200

    Add files via upload

---
+++ Lab2/create_results.sh
@@ -0,0 +1,25 @@
+echo "[Benchmarks]" > spec_results/data.ini
+
+find  spec_results/ -name "spechmmer*" |cat  >> spec_results/data.ini
+find  spec_results/ -name "speclibm*" |cat  >> spec_results/data.ini
+
+echo "[Parameters]" >> spec_results/data.ini
+
+echo "system.cpu.cpi" >> spec_results/data.ini
+echo "sim_seconds" >> spec_results/data.ini
+echo "host_seconds" >> spec_results/data.ini
+echo "system.cpu.dcache.overall_miss_rate::total" >> spec_results/data.ini
+echo "system.cpu.icache.overall_miss_rate::total" >> spec_results/data.ini
+echo "system.l2.overall_miss_rate::total" >> spec_results/data.ini
+
+echo "[Output]" >> spec_results/data.ini
+
+#Specify output file
+out="sim_results_1"
+echo "$out".txt >> spec_results/data.ini
+
+chmod +x ./read_results.sh
+./read_results.sh spec_results/data.ini
+
+mv ./spec_results/"$out".txt ./spec_results/"$out".csv 
+
--- Lab2/simulate_bench.sh
+++ Lab2/simulate_bench.sh
@@ -20,7 +20,7 @@ for l1is in ${L1IS[@]}; do
 for l2s in ${L2S[@]}; do
 for ls in ${LS[@]}; do
 
-	make $var SIZE_DCACHE="$l1ds"kB SIZE_ICACHE="$l1is"kB CACHE_LINE_SIZE="$ls" ASOC_DCACHE="$l1da" ASOC_ICACHE="$l1ia" SIZE_L2="$l2s"MB ASOC_L2="$l2a" FILE_ENDING=DL1_A_"$l1da"_S_"$l1ds"_IL1_A_"$l1ia"_S_"$l1is"_L2_A_"$l2A"_S_"$l2s"_LS_"$ls"
+	make $var SIZE_DCACHE="$l1ds"kB SIZE_ICACHE="$l1is"kB CACHE_LINE_SIZE="$ls" ASOC_DCACHE="$l1da" ASOC_ICACHE="$l1ia" SIZE_L2="$l2s"MB ASOC_L2="$l2a" FILE_ENDING=DL1_A_"$l1da"_S_"$l1ds"_IL1_A_"$l1ia"_S_"$l1is"_L2_A_"$l2a"_S_"$l2s"_LS_"$ls"
 
 done
 done
@@ -31,28 +31,8 @@ done
 done
 done
 
-echo "[Benchmarks]" > spec_results/data.ini
-
-find  spec_results/ -name "spechmmer*" |cat  >> spec_results/data.ini
-find  spec_results/ -name "speclibm*" |cat  >> spec_results/data.ini
-
-echo "[Parameters]" >> spec_results/data.ini
-
-echo "system.cpu.cpi" >> spec_results/data.ini
-echo "sim_seconds" >> spec_results/data.ini
-echo "host_seconds" >> spec_results/data.ini
-echo "system.cpu.dcache.overall_miss_rate::total" >> spec_results/data.ini
-echo "system.cpu.icache.overall_miss_rate::total" >> spec_results/data.ini
-echo "system.l2.overall_miss_rate::total" >> spec_results/data.ini
-
-echo "[Output]" >> spec_results/data.ini
-
-#Specify output file
-echo "sim_results_1.txt" >> spec_results/data.ini
-
-chmod +x ./read_results.sh
-./read_results.sh spec_results/data.ini
-
+chmod +x ./create_results.sh
+./create_results.sh
 
 
 
