2d4897c0ceaa84c5d044704df15c2f317016ede9
kprattis <68779380+kprattis@users.noreply.github.com>
2022-12-15 18:37:19 +0200

    Add files via upload

--- Lab3/create_csv.sh
+++ Lab3/create_csv.sh
@@ -1,43 +1,34 @@
 ENERGY='energies'
-OUT='mcpat_energies.txt'
 KEYWORDS=("energy" "runtime")
+COST='cost'
 
-echo '' > $OUT
-
-echo 'Simulation, Energy, Runtime' >> $OUT
-
-
+mkdir $COST
 
 FOLDS=$(ls $ENERGY/)
 
 for f in ${FOLDS[@]}
 do
+	OUT=$COST/$f-cost.txt
+	echo -n '' > $OUT
+	echo "Benchmark, $f ,Energy, Runtime" >> $OUT
 	SUBFOLDS=$(ls $ENERGY/$f/)	
 	
 	for sf in ${SUBFOLDS[@]}	
 	do
 
-		if $([[ $sf == *"spec"* ]]); then
-			echo -n $sf, >> $OUT
-			for key in ${KEYWORDS[@]}
-			do
-  				grep -oP "($key\D*)\K( (\d)+ | (\d+.\d+))" $ENERGY/$f/$sf | xargs echo -n >> $OUT
-				echo -n ',' >> $OUT
-			done
-			echo '' >> $OUT
-		else
-			SIMS=$(ls $ENERGY/$f/$sf/)
-			for sim in $SIMS
-			do
-				echo -n $sim, >> $OUT
-				for key in ${KEYWORDS[@]}
-				do
-	  				grep -oP "($key\D*)\K( (\d)+ | (\d+.\d+))" $ENERGY/$f/$sf/$sim | xargs echo -n >> $OUT
-	 				echo -n ',' >> $OUT
-				done
-				echo '' >> $OUT
-			done		
-		fi
-		
+	SIMS=$(ls $ENERGY/$f/$sf/)
+	for sim in $SIMS
+	do
+		echo $sim | grep -Po '(spec)\K\D*(?=_)' |xargs echo -n >> $OUT
+		echo $sf | grep -Po '(=)\K\d+' | xargs echo -n , >> $OUT
+		for key in ${KEYWORDS[@]}
+		do
+			grep -oP "($key\D*)\K( (\d)+ | (\d+.\d+))" $ENERGY/$f/$sf/$sim | xargs echo -n , >> $OUT
+		done
+		echo '' >> $OUT
+	done				
 	done
+
+mv $OUT $COST/$f-cost.csv
 done
+
